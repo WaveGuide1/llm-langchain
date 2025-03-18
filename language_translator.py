e00edfc8ade1c3db71e5da4867b4803fe9de7ecf
bartholomew-a-m365group <bartholomew.a@m365connect.com>
2025-03-18 16:36:20 +0100

    Language translator

---
+++ language_translator.py
@@ -0,0 +1,31 @@
+import getpass
+import os
+from dotenv import load_dotenv
+from langchain_core.messages import HumanMessage, SystemMessage
+from langchain.chat_models import init_chat_model
+from langchain_core.prompts import ChatPromptTemplate
+
+load_dotenv()
+
+if not os.getenv("GROQ_API_KEY"):
+    os.environ["GROQ_API_KEY"] = getpass.getpass(os.getenv("GROQ_API_KEY"))
+
+
+model = init_chat_model("llama3-8b-8192", model_provider="groq")
+
+system_template = "Translate the following from English into {language}"
+
+# messages = [
+#     SystemMessage("Translate the following from English into Italian"),
+#     HumanMessage("How is everything from your side my friend"),
+# ]
+
+prompt_template = ChatPromptTemplate.from_messages(
+    [("system", system_template), ("user", "{text}")]
+)
+
+prompt = prompt_template.invoke({"language": "Igbo", "text": "When are you coming"})
+
+response = model.invoke(prompt)
+print(response.content)
+
